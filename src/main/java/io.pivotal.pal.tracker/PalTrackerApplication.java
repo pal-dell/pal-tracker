96f1a9e72845b5a58fe39a990838631d10ceb7e6
PAL-8 <nilanjan.tech.roy@gmail.com>
2018-12-05 10:21:44 +0530

    actuator lab

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
@@ -21,6 +23,15 @@ public class PalTrackerApplication {
     TimeEntryRepository timeEntryRepository() {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        if(dataSource.getUrl() == null ){
+            dataSource.setUrl("jdbc:mysql://localhost:3306/tracker_dev?user=tracker&useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false");
+        }
+        dataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+        if(dataSource.getURL() == null ) {
+            dataSource.setURL("jdbc:mysql://localhost:3306/tracker_dev?user=tracker&useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false");
+        }
+        System.out.println("URL :"+dataSource.getURL());
+        System.out.println("\n\nUrl :"+dataSource.getUrl());
         return new JdbcTimeEntryRepository(dataSource);
     }
 
